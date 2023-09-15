c83f838e9f761152118c699c7d8daadebd58629d
yeogunna <yeogunna@gmail.com>
2023-09-15 17:11:15 +0900

    delivery platform

--- Course4_WebService_(JWT,Service,RabbitMQ)/ch02_delivery_platform/service/store-admin/src/main/java/org/delivery/storeadmin/domain/authorization/AuthorizationService.java
+++ Course4_WebService_(JWT,Service,RabbitMQ)/ch02_delivery_platform/service/store-admin/src/main/java/org/delivery/storeadmin/domain/authorization/AuthorizationService.java
@@ -1,2 +1,46 @@
-package org.delivery.storeadmin.domain.authorization;public class AuthorizationService {
+package org.delivery.storeadmin.domain.authorization;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.db.store.StoreEntity;
+import org.delivery.db.store.StoreRepository;
+import org.delivery.db.store.enums.StoreStatus;
+import org.delivery.db.storeuser.StoreUserEntity;
+import org.delivery.storeadmin.domain.authorization.model.UserSession;
+import org.delivery.storeadmin.domain.user.service.StoreUserService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class AuthorizationService implements UserDetailsService {
+    private final StoreUserService storeUserService;
+    private final StoreRepository storeRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<StoreUserEntity> storeUserEntity = storeUserService.getRegisterUser(username);
+        Optional<StoreEntity> storeEntity = storeRepository.findFirstByIdAndStatusOrderByIdDesc(storeUserEntity.get().getStoreId(), StoreStatus.REGISTERED);
+
+        return storeUserEntity.map(it -> {
+                    UserSession userSession = UserSession.builder()
+                            .userId(it.getId())
+                            .email(it.getEmail())
+                            .password(it.getPassword())
+                            .status(it.getStatus())
+                            .role(it.getRole())
+                            .registeredAt(it.getRegisteredAt())
+                            .unregisteredAt(it.getUnregisteredAt())
+                            .lastLoginAt(it.getLastLoginAt())
+                            .storeId(storeEntity.get().getId())
+                            .storeName(storeEntity.get().getName())
+                            .build();
+            return userSession;
+        })
+        .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
 }
--- Course4_WebService_(JWT,Service,RabbitMQ)/ch02_delivery_platform/service/store-admin/src/main/java/org/delivery/storeadmin/domain/user/business/StoreUserBusiness.java
+++ Course4_WebService_(JWT,Service,RabbitMQ)/ch02_delivery_platform/service/store-admin/src/main/java/org/delivery/storeadmin/domain/user/business/StoreUserBusiness.java
@@ -1,2 +1,33 @@
-package org.delivery.storeadmin.domain.user.business;public class StoreUserBusiness {
+package org.delivery.storeadmin.domain.user.business;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.db.store.StoreEntity;
+import org.delivery.db.store.StoreRepository;
+import org.delivery.db.store.enums.StoreStatus;
+import org.delivery.db.storeuser.StoreUserEntity;
+import org.delivery.storeadmin.domain.user.controller.model.StoreUserRegisterRequest;
+import org.delivery.storeadmin.domain.user.controller.model.StoreUserResponse;
+import org.delivery.storeadmin.domain.user.converter.StoreUserConverter;
+import org.delivery.storeadmin.domain.user.service.StoreUserService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class StoreUserBusiness {
+
+    private final StoreUserConverter storeUserConverter;
+    private final StoreUserService storeUserService;
+    private final StoreRepository storeRepository; // TODO SERVICE 로 변경하기
+
+    public StoreUserResponse register(
+            StoreUserRegisterRequest request
+    ) {
+        Optional<StoreEntity> storeEntity = storeRepository.findFirstByNameAndStatusOrderByIdDesc(request.getStoreName(), StoreStatus.REGISTERED);
+        StoreUserEntity entity = storeUserConverter.toEntity(request, storeEntity.get());
+        StoreUserEntity newEntity = storeUserService.register(entity);
+        StoreUserResponse response = storeUserConverter.toResponse(newEntity, storeEntity.get());
+        return response;
+    }
 }
