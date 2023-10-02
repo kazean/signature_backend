f40ad2857fbb38b696ee1879be98fa564f2799e6
yeogunna <yeogunna@gmail.com>
2023-10-02 14:06:40 +0900

    kotlin basic

---
+++ Course5_BackEnd(kotlin_spring)/ch02_springNkotlin/service/db/src/main/kotlin/org/delivery/db/storemenu/StoreMenuRepository.kt
@@ -0,0 +1,11 @@
+package org.delivery.db.storemenu
+/*
+
+public interface StoreMenuRepository extends JpaRepository<StoreMenuEntity, Long> {
+    // 유효한 메뉴 체크
+    Optional<StoreMenuEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, StoreMenuStatus status);
+
+    // 특정 가게의 메뉴 가져오기
+    List<StoreMenuEntity> findAllByStoreIdAndStatusOrderBySequenceDesc(Long storeId, StoreMenuStatus status);
+}
+*/
---
+++ Course5_BackEnd(kotlin_spring)/ch02_springNkotlin/service/db/src/main/kotlin/org/delivery/db/user/UserRepository.kt
@@ -0,0 +1,4 @@
+package org.delivery.db.user
+
+interface UserRepository {
+}
